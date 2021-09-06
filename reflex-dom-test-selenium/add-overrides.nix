20b938d444d06d9255a6042f1503e97a030e989c
Rune K. Svendsen <runesvend@gmail.com>
2021-09-06 09:33:53 +0200

    Disable tests for p11-kit
    See: https://github.com/NixOS/nixpkgs/issues/96715

--- reflex-dom-test-selenium/add-overrides.nix
+++ reflex-dom-test-selenium/add-overrides.nix
@@ -1,5 +1,18 @@
+let
+  # Older chromium for reflex-dom-core test suite
+  nixpkgs_oldChromium = import ../../nixpkgs-old-chromium {
+    overlays = [ (self: super: {
+      # Disable tests for p11-kit, a dependency of chromium
+      # They fail on non-NixOS systems
+      # https://github.com/NixOS/nixpkgs/issues/96715
+      p11-kit = super.p11-kit.overrideAttrs (oldAttrs: {
+        doCheck = false;
+      });
+    })];
+  };
+in
 { nixpkgs }: testPackage:
-  nixpkgs.haskell.lib.overrideCabal testPackage  
+  nixpkgs.haskell.lib.overrideCabal testPackage
     (drv: {
 
       # The headless browser run as part of the tests will exit without this
@@ -8,8 +21,9 @@
       '';
 
       testSystemDepends = with nixpkgs; (drv.testSystemDepends or []) ++ [
-        selenium-server-standalone which
-        chromium
+        nixpkgs_oldChromium.selenium-server-standalone
+        nixpkgs_oldChromium.chromium
+        which
         nixpkgs.iproute
       ];
 
